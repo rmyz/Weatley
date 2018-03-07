31ef394e79c94c6374153d7b2d9ea1ed01d9735f
Joel Coll <Ekhro97@gmail.com>
2018-03-07 18:51:32 +0100

    main layout + styling

--- Weatley.Web/src/app/components/main-layout/main-layout.module.ts
+++ Weatley.Web/src/app/components/main-layout/main-layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoreModule } from '../../core/core.module';
 
@@ -20,7 +22,9 @@ import { MainLayoutComponent } from './main-layout/main-layout.component';
     MatListModule,
     MatIconModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   exports: [MainLayoutComponent]
