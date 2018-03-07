be09088288dcaa6cc1340e20e1df84118f64f242
David Laborda <david.laborda@mataro.epiaedu.cat>
2018-03-07 20:29:34 +0100

    Merge branch 'feature/Accounting' into develop

--- Weatley.Web/src/app/components/main-components/accounting/accounting-component/accounting.component.ts
+++ Weatley.Web/src/app/components/main-components/accounting/accounting-component/accounting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountingDataService } from '../../../../core/data-services/accounting-data.service';
 
 @Component({
   selector: 'app-accounting',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountingComponent implements OnInit {
 
-  constructor() { }
+  dataAccount: [] = [];
+
+  constructor(private accountingComponent: AccountingDataService) { }
 
   ngOnInit() {
+    this.dataAccount = this.accountingComponent.getBookings();
   }
 
 }
--- Weatley.Web/src/app/components/main-components/accounting/accounting.module.ts
+++ Weatley.Web/src/app/components/main-components/accounting/accounting.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
 
 
 import { AccountingComponent } from './accounting-component/accounting.component';
@@ -13,6 +14,7 @@ import { AccountingComponent } from './accounting-component/accounting.component
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   exports: [AccountingComponent]
