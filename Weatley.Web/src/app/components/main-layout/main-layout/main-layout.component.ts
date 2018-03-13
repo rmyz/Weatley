78c28b75a5d81695601819e732df90cbb0f8b29d
Joel Coll <Ekhro97@gmail.com>
2018-03-13 20:03:45 +0100

    Header theme changer

--- Weatley.Web/src/app/components/main-layout/main-layout/main-layout.component.ts
+++ Weatley.Web/src/app/components/main-layout/main-layout/main-layout.component.ts
@@ -3,20 +3,24 @@ import { MenuItemsDataService } from '../../../core/data-services/menu-items-dat
 import { MenuItem } from '../../../core/entities/menu-item';
 
 @Component({
-  selector: 'app-main-layout',
-  templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss'],
-  providers: [MenuItemsDataService]
+    selector: 'app-main-layout',
+    templateUrl: './main-layout.component.html',
+    styleUrls: ['./main-layout.component.scss'],
+    providers: [MenuItemsDataService]
 })
 export class MainLayoutComponent implements OnInit {
 
-  menuItems: MenuItem[] = [];
-  openedSidenav = false;
+    theme = false;
+    menuItems: MenuItem[] = [];
+    openedSidenav = false;
 
-    constructor(private menuItemsDataService: MenuItemsDataService) {}
+    constructor(private menuItemsDataService: MenuItemsDataService) { }
 
-  ngOnInit() {
-    this.menuItems = this.menuItemsDataService.getMenuItems();
-  }
+    ngOnInit() {
+        this.menuItems = this.menuItemsDataService.getMenuItems();
+    }
 
+    changeTheme() {
+        this.theme = !this.theme;
+    }
 }
