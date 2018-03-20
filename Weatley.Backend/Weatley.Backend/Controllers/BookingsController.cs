ba9f4a6ab4f51970dba664896e47982e3c7db34d
Sergi Romeu <sergi.romeup@mataro.epiaedu.cat>
2018-03-20 13:05:13 +0100

    created controllers, usersContoller left

---
+++ Weatley.Backend/Weatley.Backend/Controllers/BookingsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Weatley.DataAccess;
+using Weatley.Model.Entities;
+
+namespace Weatley.Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Bookings")]
+    public class BookingsController : Controller
+    {
+        private readonly WeatleyContext _context;
+
+        public BookingsController(WeatleyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        public IEnumerable<Booking> GetBookings()
+        {
+            return _context.Bookings;
+        }
+
+        // GET: api/Bookings/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBooking([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var booking = await _context.Bookings.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(booking);
+        }
+
+        // PUT: api/Bookings/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBooking([FromRoute] Guid id, [FromBody] Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != booking.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bookings
+        [HttpPost]
+        public async Task<IActionResult> PostBooking([FromBody] Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBooking", new { id = booking.Id }, booking);
+        }
+
+        // DELETE: api/Bookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var booking = await _context.Bookings.SingleOrDefaultAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+
+            return Ok(booking);
+        }
+
+        private bool BookingExists(Guid id)
+        {
+            return _context.Bookings.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Weatley.Backend/Weatley.Backend/Controllers/OrdersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Weatley.DataAccess;
+using Weatley.Model.Entities;
+
+namespace Weatley.Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Orders")]
+    public class OrdersController : Controller
+    {
+        private readonly WeatleyContext _context;
+
+        public OrdersController(WeatleyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public IEnumerable<Order> GetOrders()
+        {
+            return _context.Orders;
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var order = await _context.Orders.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder([FromRoute] Guid id, [FromBody] Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<IActionResult> PostOrder([FromBody] Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var order = await _context.Orders.SingleOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        private bool OrderExists(Guid id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
