82b22cdfcde46f03af0121f0edbaded557298321
Joel Coll <Ekhro97@gmail.com>
2018-03-25 21:45:57 +0200

    Registration api working

--- Weatley.Backend/Weatley.Backend/Startup.cs
+++ Weatley.Backend/Weatley.Backend/Startup.cs
@@ -26,7 +26,6 @@ namespace Weatley.Backend
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(Configuration);
