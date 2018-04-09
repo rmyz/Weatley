ae66f40b6305d649489b9351dd3d502189485449
David Laborda <david.laborda@mataro.epiaedu.cat>
2018-04-09 13:07:01 +0200

    grid list for dashboard first attempt WIP

--- Weatley.Web/src/app/components/main-components/dashboard/dashboard.module.ts
+++ Weatley.Web/src/app/components/main-components/dashboard/dashboard.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { DashboardComponent } from './dashboard-component/dashboard.component';
 
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		MatGridListModule
 	],
 	declarations: [
 		DashboardComponent
+	],
+	exports: [
+		MatGridListModule
 	]
 })
 export class DashboardModule { }
