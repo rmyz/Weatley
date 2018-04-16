82cb9517993ef28da4bf36b1a4462275ceb1b7f9
David Laborda <david.laborda@mataro.epiaedu.cat>
2018-04-16 13:29:36 +0200

    Dash board layout, function wip

--- Weatley.Web/src/app/components/main-components/dashboard/dashboard-component/dashboard.component.ts
+++ Weatley.Web/src/app/components/main-components/dashboard/dashboard-component/dashboard.component.ts
@@ -8,18 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
 
-	tiles: any[];
-
-
 	constructor() { }
 
 	ngOnInit() {
-		this.tiles = [
-			{ text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-			{ text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-			{ text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-			{ text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
-		];
 	}
 
 }
