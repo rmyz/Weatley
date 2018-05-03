61a4788b7d93f6f33db10e831532b01db7783501
David Laborda <david.laborda@mataro.epiaedu.cat>
2018-05-03 13:44:19 +0200

    Info page added, WIP services and icons

--- Weatley.App/app/pages/info/info.component.ts
+++ Weatley.App/app/pages/info/info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { HotelDataService } from "~/core/data-services/hotel-data.service";
+import { Hotel } from "~/core/entities/hotel";
 
 /* ***********************************************************
 * Before you can navigate to this page from your app, you need to reference this page's module in the
@@ -12,19 +14,20 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 @Component({
 	selector: "Info",
 	moduleId: module.id,
-	templateUrl: "./info.component.html"
+	templateUrl: "./info.component.html",
+	styleUrls: ["./info.component.scss"],
+	providers: [HotelDataService]
 })
 export class InfoComponent implements OnInit {
-	constructor() {
-		/* ***********************************************************
-		* Use the constructor to inject app services that you need in this component.
-		*************************************************************/
-	}
+
+	private hotel: Hotel = new Hotel();
+
+	constructor(private hotelDataService: HotelDataService) { }
 
 	ngOnInit(): void {
-		/* ***********************************************************
-		* Use the "ngOnInit" handler to initialize data for this component.
-		*************************************************************/
+		this.hotelDataService.getHotel().subscribe((hotel) => {
+			this.hotel = hotel;
+		});
 	}
 
 	onDrawerButtonTap(): void {
