39aa2725b9da74d20e14d932aca3f31276105e0a
Sergi Romeu <sergi.romeup@mataro.epiaedu.cat>
2018-05-03 17:24:25 +0200

    styling on tabs

--- Weatley.App/app/pages/order-new/order-new.component.ts
+++ Weatley.App/app/pages/order-new/order-new.component.ts
@@ -4,7 +4,8 @@ import { RouterExtensions } from "nativescript-angular/router";
 @Component({
 	selector: "OrderNew",
 	moduleId: module.id,
-	templateUrl: "./order-new.component.html"
+	templateUrl: "./order-new.component.html",
+	styleUrls: ["./order-new.component.scss"]
 })
 export class OrderNewComponent implements OnInit {
 	constructor(private routerExtensions: RouterExtensions) { }
