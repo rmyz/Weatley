d64becd395c5345e86fb5d274d3062c8353950f0
Sergi Romeu <sergi.romeup@mataro.epiaedu.cat>
2018-05-05 20:43:00 +0200

    filling cart on tap working

--- Weatley.App/app/pages/order-new/order-new.component.ts
+++ Weatley.App/app/pages/order-new/order-new.component.ts
@@ -2,21 +2,35 @@ import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { ProductDataService } from "~/core/data-services/product-data.service";
 import { Product } from "~/core/entities/product";
+import { Order } from "~/core/entities/order";
+import { ProductOrdered } from "~/core/entities/product-ordered";
+import { Customer } from "~/core/entities/customer";
+import { CustomerDataService } from "~/core/data-services/customer-data.service";
+
+const uuidv4 = require("uuid/v4");
 
 @Component({
 	selector: "OrderNew",
 	moduleId: module.id,
 	templateUrl: "./order-new.component.html",
 	styleUrls: ["./order-new.component.scss"],
-	providers: [ProductDataService]
+	providers: [ProductDataService, CustomerDataService]
 })
 export class OrderNewComponent implements OnInit {
 	private foodItems: Array<Product> = [];
 	private drinkItems: Array<Product> = [];
 	private serviceItems: Array<Product> = [];
 
+	private comment: string = "";
+	private finalPrice: number = 0;
+	private orderId = uuidv4();
+	private orderItems: Order = new Order();
+	private customer: Customer;
+	private customerId = "ed90a54c-d224-49aa-8046-f88ba013f854";
+
 	constructor(private routerExtensions: RouterExtensions,
-				private productDataService: ProductDataService) { }
+				private productDataService: ProductDataService,
+				private customerDataService: CustomerDataService) { }
 
 	ngOnInit(): void {
 		this.productDataService.getProduct().subscribe((products) => {
@@ -32,6 +46,9 @@ export class OrderNewComponent implements OnInit {
 				}
 			});
 		});
+		this.customerDataService.getCustomerById(this.customerId).subscribe((customer) => {
+			this.customer = customer;
+		});
 	}
 
 	goBack() {
@@ -39,17 +56,143 @@ export class OrderNewComponent implements OnInit {
 	}
 
 	onTapFood(food) {
-		console.log(food.index);
-		console.log(this.foodItems[food.index]);
+		if (this.orderItems.productsOrdered) {
+			let changed = false;
+			const found = this.orderItems.productsOrdered.find(p => p.product.name === this.foodItems[food.index].name);
+			if (found) {
+				found.quantity += 1;
+				changed = true;
+				this.finalPrice = this.finalPrice + this.foodItems[food.index].price;
+				this.orderItems.finalPrice = this.finalPrice;
+			} else {
+				this.finalPrice = this.finalPrice + this.foodItems[food.index].price;
+				this.orderItems.productsOrdered.push(new ProductOrdered({
+					id: uuidv4(),
+					order: new Order({
+						id: this.orderId,
+						orderDate: new Date(),
+						customer: this.customer,
+						comment: this.comment,
+						finalPrice: this.finalPrice
+					}),
+					product: this.foodItems[food.index],
+					quantity: 1
+				}));
+			}
+		} else {
+			this.orderItems.productsOrdered = new Array<ProductOrdered>();
+			this.finalPrice = this.foodItems[food.index].price;
+			this.orderItems.productsOrdered.push(new ProductOrdered({
+				id: uuidv4(),
+				order: new Order({
+					id: this.orderId,
+					orderDate: new Date(),
+					customer: this.customer,
+					comment: this.comment,
+					finalPrice: this.finalPrice
+				}),
+				product: this.foodItems[food.index],
+				quantity: 1
+			}));
+		}
+		this.updateOrder();
 	}
 
 	onTapDrink(drink) {
-		console.log(drink.index);
-		console.log(this.drinkItems[drink.index]);
+		if (this.orderItems.productsOrdered) {
+			let changed = false;
+			const found = this.orderItems.productsOrdered.find(p => p.product.name === this.drinkItems[drink.index].name);
+			if (found) {
+				found.quantity += 1;
+				changed = true;
+				this.finalPrice = this.finalPrice + this.drinkItems[drink.index].price;
+				this.orderItems.finalPrice = this.finalPrice;
+			} else {
+				this.finalPrice = this.finalPrice + this.drinkItems[drink.index].price;
+				this.orderItems.productsOrdered.push(new ProductOrdered({
+					id: uuidv4(),
+					order: new Order({
+						id: this.orderId,
+						orderDate: new Date(),
+						customer: this.customer,
+						comment: this.comment,
+						finalPrice: this.finalPrice
+					}),
+					product: this.drinkItems[drink.index],
+					quantity: 1
+				}));
+			}
+		} else {
+			this.orderItems.productsOrdered = new Array<ProductOrdered>();
+			this.finalPrice = this.drinkItems[drink.index].price;
+			this.orderItems.productsOrdered.push(new ProductOrdered({
+				id: uuidv4(),
+				order: new Order({
+					id: this.orderId,
+					orderDate: new Date(),
+					customer: this.customer,
+					comment: this.comment,
+					finalPrice: this.finalPrice
+				}),
+				product: this.drinkItems[drink.index],
+				quantity: 1
+			}));
+		}
+		this.updateOrder();
 	}
 
 	onTapService(service) {
-		console.log(service.index);
-		console.log(this.serviceItems[service.index]);
+		if (this.orderItems.productsOrdered) {
+			let changed = false;
+			const found = this.orderItems.productsOrdered.find(p => p.product.name === this.serviceItems[service.index].name);
+			if (found) {
+				found.quantity += 1;
+				changed = true;
+				this.finalPrice = this.finalPrice + this.serviceItems[service.index].price;
+				this.orderItems.finalPrice = this.finalPrice;
+			} else {
+				this.finalPrice = this.finalPrice + this.serviceItems[service.index].price;
+				this.orderItems.productsOrdered.push(new ProductOrdered({
+					id: uuidv4(),
+					order: new Order({
+						id: this.orderId,
+						orderDate: new Date(),
+						customer: this.customer,
+						comment: this.comment,
+						finalPrice: this.finalPrice
+					}),
+					product: this.serviceItems[service.index],
+					quantity: 1
+				}));
+			}
+		} else {
+			this.orderItems.productsOrdered = new Array<ProductOrdered>();
+			this.finalPrice = this.serviceItems[service.index].price;
+			this.orderItems.productsOrdered.push(new ProductOrdered({
+				id: uuidv4(),
+				order: new Order({
+					id: this.orderId,
+					orderDate: new Date(),
+					customer: this.customer,
+					comment: this.comment,
+					finalPrice: this.finalPrice
+				}),
+				product: this.serviceItems[service.index],
+				quantity: 1
+			}));
+		}
+		this.updateOrder();
+	}
+
+	updateOrder() {
+		this.orderItems.customer = this.customer;
+		this.orderItems.id = this.orderId;
+		this.orderItems.finalPrice = this.finalPrice;
+		this.orderItems.comment = this.comment;
+		this.orderItems.status = "pending";
+	}
+
+	onTapCartItem(cartItem) {
+
 	}
 }
