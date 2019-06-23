b50f67b55fb3a36ccf38a77355ff9e869edd2720
Sergi Romeu <romeuhd@gmail.com>
2019-06-23 18:49:25 +0200

    add auth on calls for graphql

--- Weatley.Web/src/app/graphql.module.ts
+++ Weatley.Web/src/app/graphql.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
 
 const uri = 'http://localhost:4000';
 export function createApollo(httpLink: HttpLink) {
+  const authLink = new ApolloLink((operation, forward) => {
+    const token = sessionStorage.getItem('access_token');
+
+    operation.setContext({
+      headers: {
+        Authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+
+    return forward(operation);
+  });
+
   return {
-    link: httpLink.create({ uri }),
+    link: authLink.concat(httpLink.create({ uri })),
     cache: new InMemoryCache(),
   };
 }
